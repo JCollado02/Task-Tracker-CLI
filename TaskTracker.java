99d000e48050a76026e6fe33af603a37bc37c442
JCollado02 <ji.sito122@gmail.com>
2025-08-13 22:06:15 -0400

    Added json file creation and updating

--- TaskTracker.java
+++ TaskTracker.java
@@ -1,3 +1,9 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class TaskTracker {
@@ -7,24 +13,56 @@ public class TaskTracker {
         return title;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // create the data folder with data.json file to store list input
+        String JSON_FILE = "data/data.json";
+
+        File folder = new File("data");
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+
+        File jsonFile = new File(JSON_FILE);
+
         if (args.length == 0) {
             System.out.println("No arguments given");
             return;
         }
-
+        // ----------------------------------------------------------------------------------------------------------------------------------//
+        // switch for add, delete, update and list commands
         switch (args[0].toLowerCase()) {
             case "add":
-                if (args.length < 2) {
+                if (args.length < 2) { // if no entry after "add", then title missing
                     System.out.println("Invalid Task Title");
-                    break;
-                } else {
-                    String title = extractTitle(args, 1);
-                    System.out.println("Added: " + title);
                     break;
                 }
 
+                // extract title from args
+                String title = extractTitle(args, 1).replace("\\", "\\\\").replace("\"", "\\\"");
+
+                // document id, so we an increment for each entry, get latest one from json, if
+                // starting new list use 0
+                Path idPath = Paths.get("data/.id");
+                int nextId = (Files.exists(idPath) ? Integer.parseInt(Files.readString(idPath).trim()) + 1 : 0);
+
+                Files.writeString(idPath, String.valueOf(nextId)); // store latest id
+
+                try (FileWriter w = new FileWriter("data/data.json", true)) { // store added task
+                    w.write("{ \"id\": " + nextId + ", \"task\": \"" + title + "\" }\n");
+                }
+                System.out.println("Task added (ID: " + nextId + ")");
+                break;
+
+            // -------------------------------------------------------------------------------------------------------------------------------------//
+            // removes specified task, behaves similarly to add
             case "delete":
+                if (args.length < 2) { // if no entry after "add", then title missing
+                    System.out.println("Invalid id");
+                    break;
+                }
+
+                // TODO: Ensure we only get integers for ids, check if valid id, then somehow
+                // use it to select which list item we delete
                 System.out.println("You gave a delete command");
                 break;
 
